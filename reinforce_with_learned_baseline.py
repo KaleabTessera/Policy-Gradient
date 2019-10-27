f38c23b5a2d593363a57d8f566e689b4cfa46aca
Kale-ab <kaleabtessera@gmail.com>
2019-10-27 22:05:10 +0200

    Implemeted reinforce with learned baseline.

--- reinforce_with_learned_baseline.py
+++ reinforce_with_learned_baseline.py
@@ -13,16 +13,56 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class PolicyValueNetwork(nn.Module):
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, input_size=8, hidden_size=128,
+                 num_hidden_layers=0, policy_output_size=4, value_output_size=1):
+        super(PolicyValueNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu1 = nn.ReLU()
+        self.hidden_layers = nn.ModuleList()
+        
+        # Hidden layers
+        for i in np.arange(num_hidden_layers - 1):
+            self.hidden_layers.append(nn.Linear(
+                hidden_size, hidden_size))
+            self.hidden_layers.append(nn.ReLU())
+
+
+        # Define simple policy head
+        self.policy = nn.Sequential(
+            nn.Linear(hidden_size,
+                      policy_output_size))
+
+        # Define simple value head
+        self.value = nn.Sequential(
+            nn.Linear(hidden_size,
+                      value_output_size))
 
     def forward(self, x):
-        raise NotImplementedError
-        return dist, value
+        out = self.fc1(x)
+        out = self.relu1(out)
+        for layer in self.hidden_layers:
+            out = layer(out)
+        probs = Categorical(logits=self.policy(out))
+        return probs, self.value(out)
+
+    def act(self, state):
+        state = torch.from_numpy(state).float().to(device)
+        action_distribution, value = self.forward(state)
+        action = action_distribution.sample()
+        return action.item(), action_distribution.log_prob(action), value
+
 
 
 def compute_returns(rewards, gamma):
-    raise NotImplementedError
+    r = 0
+    returns = []
+    for step in reversed(range(len(rewards))):
+        r = rewards[step] + gamma * r
+        returns.insert(0, r)
+    returns = np.array(returns)
+    mean = returns.mean(axis=0)
+    std = returns.std(axis=0)
+    returns = (returns - mean)/std
     return returns
 
 
@@ -36,14 +76,61 @@ def reinforce_learned_baseline(env, policy_model, seed, learning_rate,
     env.seed(seed)
     random.seed(seed)
 
-    # if episode % 50 == 0 and verbose:
-    #     print('Episode {}\tAverage Score: {:.2f}'.format(episode, np.mean(scores_deque)))
-    raise NotImplementedError
-    return policy_value_net, scores
+    max_episode_length = 1000
+
+    scores = []
+    policy_model = policy_model.to(device)
+    optimizer = optim.Adam(policy_model.parameters(), lr=learning_rate)
+    scores_deque = deque(maxlen=100)
+    for e in np.arange(start=1, stop=number_episodes+1):
+        saved_probs = []
+        saved_value = []
+        rewards = []
+        policy_loss = []
+        state = env.reset()
+        for i in np.arange(start=1, stop=max_episode_length+1):
+            action, action_log_probs, value = policy_model.act(state)
+            state, reward, done, _ = env.step(action)
+            rewards.append(reward)
+            saved_value.append(value)
+            saved_probs.append(action_log_probs.unsqueeze(0))
+            if done:
+                break
+
+        total_rewards = sum(rewards)
+        scores.append(total_rewards)
+        scores_deque.append(total_rewards)
+
+        G = compute_returns(rewards, gamma)
+        G = torch.from_numpy(G).float().to(device)
+
+        total_value = torch.cat(saved_value)
+        saved_probs = torch.cat(saved_probs)
+        delta = G - total_value
+
+        # Loss for policy
+        policy_loss = -torch.sum(saved_probs*delta.detach())
+
+        # Loss for value
+        value_loss = 0.5*torch.sum(delta**2)
+        # value_loss = torch.sum(torch.abs(delta))
+
+        # compute the composite loss
+        loss = policy_loss + value_loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if e % 50 == 0 and verbose:
+            print('Episode {}\tAverage Score: {:.2f}'.format(
+                e, np.mean(scores_deque)))
+    return policy_model, scores
 
 
 def main():
     env = gym.make('LunarLander-v2')
+
     print('Action space: ', env.action_space)
     print('Observation space: ', env.observation_space)
 
@@ -59,6 +146,9 @@ def main():
                                              number_episodes,
                                              gamma, verbose=True)
 
+    # Record trained net
+    env = gym.wrappers.Monitor(
+        env, './video/', force=True)
     state = env.reset()
     for t in range(2000):
         state = torch.from_numpy(state).float().to(device)
